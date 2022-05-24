8b08e660a87e96c38c7f5e5d7f110d694b2000b4
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-05-24 16:45:54 +0800

    get file ID

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Emoji"
+__version__ = "1.0.0"
+__url__ = "https://github.com/Emojigit/catworm"
+__description__ = "Get file ID"
+__dname__ = "get_file_id"
+
+from telethon import events, utils
+def setup(bot):
+    @bot.on(events.NewMessage(pattern='/fileid'))
+    async def catworm(event):
+        replymsg = await event.get_reply_message()
+        if replymsg and replymsg.document != None:
+            async with bot.action(event.chat, 'typing') as action:
+                id = utils.pack_bot_file_id(replymsg.document)
+                if id != None:
+                    await replymsg.reply("`{}`".format(id))
+                else:
+                    await replymsg.reply("Got file, but error.")
+                raise events.StopPropagation
+        await event.respond("Please reply to a valid file.")
+        raise events.StopPropagation
